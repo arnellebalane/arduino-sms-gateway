20d87fe85d5e438c4ad4436cbb3f2e7e744e98f2
Arnelle Balane <arnellebalane@gmail.com>
2023-06-07 22:45:12 -0700

    Send authenticated request from wifi-module

--- wifi-module/wifi-module.ino
+++ wifi-module/wifi-module.ino
@@ -1,3 +1,4 @@
+#include <Base64.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClientSecureBearSSL.h>
@@ -8,34 +9,30 @@
 #define URL ""
 #define FINGERPRINT ""
 
+const String API_USERNAME = "";
+const String API_PASSWORD = "";
+
 void setup() {
     Serial.begin(115200);
-
-    Serial.print("Connecting to network");
-    WiFi.begin(SSID, PASSWORD);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println();
-    Serial.println("Connected to network");
-
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
+    ensureNetworkConnection();
 }
 
 void loop() {
-    String line = Serial.readStringUntil('\n');
-    if (line.length() > 0) {
-        Serial.println(">>> " + line);
+    String message = Serial.readStringUntil('\n');
+    message.trim();
 
+    if (message.length() > 0) {
+        Serial.println(">>> " + message);
+
+        ensureNetworkConnection();
         WiFiClientSecure client;
         client.setFingerprint(FINGERPRINT);
 
         HTTPClient http;
         http.begin(client, URL);
+        http.addHeader("Authorization", getAuthorizationHeader());
 
-        String payload = "{\"input\":\"" + line + "\"}";
+        String payload = "{\"input\":\"" + message + "\"}";
         int httpCode = http.POST(payload);
 
         if (httpCode == HTTP_CODE_OK) {
@@ -47,4 +44,24 @@ void loop() {
 
         http.end();
     }
+}
+
+void ensureNetworkConnection() {
+    if (WiFi.status() != WL_CONNECTED) {
+        Serial.print("Connecting to network");
+        WiFi.begin(SSID, PASSWORD);
+        while (WiFi.status() != WL_CONNECTED) {
+            delay(500);
+            Serial.print(".");
+        }
+        Serial.println();
+        Serial.println("Connected to network");
+
+        Serial.print("IP address: ");
+        Serial.println(WiFi.localIP());
+    }
+}
+
+String getAuthorizationHeader() {
+    return "Basic " + base64::encode(API_USERNAME + ":" + API_PASSWORD);
 }
