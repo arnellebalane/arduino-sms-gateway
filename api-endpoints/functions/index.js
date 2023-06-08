a2acf5c7a623265a41014072378efc8a524aaa18
Arnelle Balane <arnellebalane@gmail.com>
2023-06-07 23:27:26 -0700

    Use Authorization header to authorize API endpoint requests

--- api-endpoints/functions/index.js
+++ api-endpoints/functions/index.js
@@ -21,44 +21,61 @@ exports.receiveMessage = functions
   .runWith({ secrets: [FIREBASE_API_KEY] })
   .https.onRequest(async (request, response) => {
     /**
-     * Expected request payload type:
-     *  {
-     *    email: string;
-     *    password: string;
-     *    payload: any;
-     *  }
+     * Requests to this function should be authorized using Basic auth. This means there should be an Authorization
+     * header with value "Basic base64(email:password)"
      */
-    const { email, password, payload } = request.body;
-
-    /**
-     * The Firebase Admin SDK does not support signing in using a user's credentials. Alternatively, we could use the
-     * Firebase JavaScript (NodeJS) SDK to perform the authentication, but the REST API ended up being the easier
-     * option for our use-case. https://firebase.google.com/docs/reference/rest/auth
-     */
-    const signInEndpoint = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY.value()}`;
-
+    const credential = getCredentials(request.headers['authorization']);
     try {
-      /**
-       * Attempt to sign-in using the user's credentials. We don't care about the response of the request, if it
-       * succeeds then it is a valid user and we proceed with function execution.
-       */
-      const signInPayload = { email, password, returnSecureToken: true };
-      await axios.post(signInEndpoint, signInPayload, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+      await authenticateUser(credential);
     } catch (error) {
+      logger.error(error, { structuredClone: true });
+      logger.debug('Invalid user credentials.');
       return response.status(403).end();
     }
 
     try {
-      const doc = await admin.firestore().collection('messages').add(payload);
+      const doc = await admin.firestore().collection('messages').add(request.body);
       logger.info(`New message stored: ${doc.id}`);
       response.status(200).end();
     } catch (error) {
+      logger.debug(error, { structuredClone: true });
       return response.status(500).end();
     }
 
     return response.status(200).end();
   });
+
+function getCredentials(authorization) {
+  if (!authorization) {
+    logger.debug('Authorization header not found.');
+    return response.status(403).send();
+  }
+  const [type, token] = authorization?.split(' ') ?? [];
+  if (type !== 'Basic') {
+    logger.debug(`Authorization type "${type}" is not supported.`);
+    return response.status(403).send();
+  }
+  const buffer = Buffer.from(token, 'base64');
+  const [email, password] = buffer.toString('ascii').split(':');
+  return { email, password };
+}
+
+async function authenticateUser({ email, password }) {
+  /**
+   * The Firebase Admin SDK does not support signing in using a user's credentials. Alternatively, we could use the
+   * Firebase JavaScript (NodeJS) SDK to perform the authentication, but the REST API ended up being the easier option
+   * for our use-case. https://firebase.google.com/docs/reference/rest/auth
+   */
+  const signInEndpoint = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY.value()}`;
+
+  /**
+   * Attempt to sign-in using the user's credentials. We don't care about the response of the request, if it succeeds
+   * then it is a valid user and we proceed with function execution.
+   */
+  const signInPayload = { email, password, returnSecureToken: true };
+  await axios.post(signInEndpoint, signInPayload, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
